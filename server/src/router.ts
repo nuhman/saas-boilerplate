9eea7a77ea4a9f71db981ea9c3e9a39399625b29
nuhman <nuhman7@outlook.com>
2025-11-09 14:26:01 +0530

    Remove message route access from UI

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,30 +1,7 @@
-import { publicProcedure, router } from "./trpc";
+import { router } from "./trpc";
 import { movieRouter } from "./routers/movie.router";
-import { z } from "zod";
 
 export const appRouter = router({
-  sayHello: publicProcedure
-    .input(z.object({ name: z.string().optional() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.name ?? "world"}`,
-      };
-    }),
-
-  getMessages: publicProcedure.query(async ({ ctx }) => {    
-    return ctx.prisma.message.findMany({
-      orderBy: { createdAt: "desc" },
-    });
-  }),
-
-  createMessage: publicProcedure
-    .input(z.object({ content: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      return ctx.prisma.message.create({
-        data: { content: input.content },
-      });
-    }),
-
   movie: movieRouter,
 });
 
